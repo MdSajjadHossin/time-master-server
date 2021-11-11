006f4e4a550097ec6e14c52364f1071f33a9de02
MdSajjadHossin <iamsajjadhossin@gmail.com>
2021-11-12 00:37:15 +0600

    detele api added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -33,13 +34,30 @@ async function run(){
             const explores = await cursor.toArray();
             res.send(explores);
         })
-        //Order Post API
+        //Order POST API
         app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order)
             console.log(order);
             res.json(result);
         });
+        //Order GET API
+        app.get('/orders', async (req, res) =>{
+            const email = req.query.email;
+            const query = {email: email}
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.json(orders);
+        })
+        //Oredr DELETE API
+        app.delete('/orders/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+
+            console.log('Deleting User With Id:',result);
+            res.json(result);
+        })
     }
     finally{
         // await client.close();
